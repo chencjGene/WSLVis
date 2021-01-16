a2ee6d84e070aadbba463f34accdae0586d65999
chencjGene <chencjgene@gmail.com>
2021-01-16 16:17:33 +0800

    create sub_component files

---
+++ vis/src/plugins/image_card.js
@@ -0,0 +1,21 @@
+const ImageCards = function(parent){
+    let that =this;
+    that.parent = parent;
+
+    this.sub_component_update = function() {
+
+    };
+
+    this.create = function() {
+
+    };
+
+    this.update = function(){
+
+    };
+
+    this.remove = function(){
+
+    };
+
+}
---
+++ vis/src/plugins/text_tree.js
@@ -0,0 +1,20 @@
+const TextTree = function(parent){
+    let that =this;
+    that.parent = parent;
+
+    this.sub_component_update = function() {
+
+    };
+
+    this.create = function() {
+
+    };
+
+    this.update = function(){
+
+    };
+
+    this.remove = function(){
+
+    };
+}
