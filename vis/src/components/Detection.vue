b18007a20da822cb114c313523e238c057106716
chencjgene <chencjgene@gmail.com>
2021-07-06 10:22:26 +0800

    add label hierarchy and image cluster

--- vis/src/components/Detection.vue
+++ vis/src/components/Detection.vue
@@ -53,6 +53,7 @@ import {
 } from "../plugins/layout_text";
 import {image_cluster_list_layout} from "../plugins/layout_image";
 import {ConnectionLayout} from "../plugins/layout_connection";
+import { svgDropDown } from "../plugins/svg_drop_down.js";
 
 // import { SetManager } from "../plugins/set_manager";
 
@@ -276,243 +277,34 @@ export default {
                 .style("opacity", (d) => (d.target.mini_selected ? 1 : 0));
         },
         legend_create() {
-            /**  svg-dropdown.js - svg dropdown library  */
-            function svgDropDown(options) {
-                if (typeof options !== 'object' || options === null || !options.container) {
-                  console.error(new Error("Container not provided"));
-                  return;
-                }
-                const defaultOptions = {
-                  width: 200,
-                  members: [],
-                  fontSize: 12,
-                  color: "#333",
-                  fontFamily: "Calibri,Candara,Segoe,Segoe UI,Optima,Arial,sans-serif",
-                  x: 0,
-                  y: 0,
-                  changeHandler: function() {}
-                };
-
-                options = { ...defaultOptions,
-                  ...options
-                };
-
-                options.optionHeight = options.fontSize * 2;
-                options.height = options.fontSize + 8;
-                options.padding = 5;
-                options.hoverColor = "#0c56f5";
-                options.hoverTextColor = "#fff";
-                options.bgColor = "#fff";
-                options.width = options.width - 2;
-
-                const g = options.container
-                  .append("svg")
-                  .attr("x", options.x)
-                  .attr("y", options.y)
-                  .attr("shape-rendering", "crispEdges")
-                  .append("g")
-                  .attr("transform", "translate(1,1)")
-                  .attr("font-family", options.fontFamily);
-
-                let selectedOption =
-                  options.members.length === 0 ? {
-                    label: "",
-                    value: ""
-                  } :
-                  options.members[options.initMember];
-
-                /** Rendering Select Field */
-                const selectField = g.append("g");
-
-                // background
-                selectField
-                  .append("rect")
-                  .attr("width", options.width)
-                  .attr("height", options.height)
-                  .attr("class", "option select-field")
-                  .attr("fill", options.bgColor)
-                  .style("stroke", "#a0a0a0")
-                  .style("stroke-width", "1");
-
-                // text
-                const activeText = selectField
-                  .append("text")
-                  .text(selectedOption.label)
-                  .attr("x", options.padding)
-                  .attr("y", options.height / 2 + options.fontSize / 3)
-                  .attr("font-size", options.fontSize)
-                  .attr("fill", options.color);
-
-                // arrow symbol at the end of the select box
-                selectField
-                  .append("text")
-                  .text("▼")
-                  .attr("x", options.width - options.fontSize - options.padding)
-                  .attr("y", options.height / 2 + (options.fontSize - 2) / 3)
-                  .attr("font-size", options.fontSize - 2)
-                  .attr("fill", options.color);
-
-                // transparent surface to capture actions
-                selectField
-                  .append("rect")
-                  .attr("width", options.width)
-                  .attr("height", options.height)
-                  .style("fill", "transparent")
-                  .on("click", handleSelectClick);
-
-                /** rendering options */
-                const optionGroup = g
-                  .append("g")
-                  .attr("transform", `translate(0, ${options.height})`)
-                  .attr("opacity", 0); //.attr("display", "none"); Issue in IE/Firefox: Unable to calculate textLength when display is none.
-
-                // Rendering options group
-                const optionEnter = optionGroup
-                  .selectAll("g")
-                  .data(options.members)
-                  .enter()
-                  .append("g")
-                  .on("click", handleOptionClick);
-
-                // Rendering background
-                optionEnter
-                  .append("rect")
-                  .attr("width", options.width)
-                  .attr("height", options.optionHeight)
-                  .attr("y", function(d, i) {
-                    return i * options.optionHeight;
-                  })
-                  .attr("class", "option")
-                  .style("stroke", options.hoverColor)
-                  .style("stroke-dasharray", (d, i) => {
-                    let stroke = [
-                      0,
-                      options.width,
-                      options.optionHeight,
-                      options.width,
-                      options.optionHeight
-                    ];
-                    if (i === 0) {
-                      stroke = [
-                        options.width + options.optionHeight,
-                        options.width,
-                        options.optionHeight
-                      ];
-                    } else if (i === options.members.length - 1) {
-                      stroke = [0, options.width, options.optionHeight * 2 + options.width];
-                    }
-                    return stroke.join(" ");
-                  })
-                  .style("stroke-width", 1)
-                  .style("fill", options.bgColor);
-
-                // Rendering option text
-                optionEnter
-                  .append("text")
-                  .attr("x", options.padding)
-                  .attr("y", function(d, i) {
-                    return (
-                      i * options.optionHeight +
-                      options.optionHeight / 2 +
-                      options.fontSize / 3
-                    );
-                  })
-                  .text(function(d) {
-                    return d.label;
-                  })
-                  .attr("font-size", options.fontSize)
-                  .attr("fill", options.color)
-                  .each(wrap);
-
-                // Rendering option surface to take care of events
-                optionEnter
-                  .append("rect")
-                  .attr("width", options.width)
-                  .attr("height", options.optionHeight)
-                  .attr("y", function(d, i) {
-                    return i * options.optionHeight;
-                  })
-                  .style("fill", "transparent")
-                  .on("mouseover", handleMouseOver)
-                  .on("mouseout", handleMouseOut);
-
-                //once the textLength gets calculated, change opacity to 1 and display to none
-                optionGroup.attr("display", "none").attr("opacity", 1);
-
-                d3.select("body").on("click", function() {
-                  optionGroup.attr("display", "none");
-                });
-
-                // Utility Methods
-                function handleMouseOver(event) {
-                  d3.select(event.target.parentNode)
-                    .select(".option")
-                    .style("fill", options.hoverColor);
-
-                  d3.select(event.target.parentNode)
-                    .select("text")
-                    .style("fill", options.hoverTextColor);
-                }
-
-                function handleMouseOut(event) {
-                  d3.select(event.target.parentNode)
-                    .select(".option")
-                    .style("fill", options.bgColor);
-
-                  d3.select(event.target.parentNode)
-                    .select("text")
-                    .style("fill", options.color);
-                }
-
-                function handleOptionClick(event, d) {
-                  event.stopPropagation();
-                  selectedOption = d;
-                  activeText.text(selectedOption.label).each(wrap);
-                  typeof options.changeHandler === 'function' && options.changeHandler.call(this, d);
-                  optionGroup.attr("display", "none");
-                }
-
-                function handleSelectClick(event) {
-                    console.log(event);
-                  event.stopPropagation();
-                  const visibility = optionGroup.attr("display") === "block" ? "none" : "block";
-                  optionGroup.attr("display", visibility);
-                }
-
-                // wraps words
-                function wrap() {
-                  const width = options.width;
-                  const padding = options.padding;
-                  const self = d3.select(this);
-                  let textLength = self.node().getComputedTextLength();
-                  let text = self.text();
-                  const textArr = text.split(/\s+/);
-                  let lastWord = "";
-                  while (textLength > width - 2 * padding && text.length > 0) {
-                    lastWord = textArr.pop();
-                    text = textArr.join(" ");
-                    self.text(text);
-                    textLength = self.node().getComputedTextLength();
-                  }
-                  self.text(text + " " + lastWord);
-
-                  // providing ellipsis to last word in the text
-                  if (lastWord) {
-                    textLength = self.node().getComputedTextLength();
-                    text = self.text();
-                    while (textLength > width - 2 * padding && text.length > 0) {
-                      text = text.slice(0, -1);
-                      self.text(text + "...");
-                      textLength = self.node().getComputedTextLength();
-                    }
-                  }
-                }
-              }
 
             let that = this;
-            let top_y = 4;
-            let drop_down_width = 100;
-            let start_x = 15;
+        
+            let title1_x = 20;
+            let title1_len = 100;
+            let title2_x = 350;
+            let title2_len = 100;
+
+            let top_y = 8;
+            let drop_down_width = 90;
+            let start_x = title1_x + title1_len + 80;
+
+
+            // titles
+            that.svg.append("text")
+                .attr("class", "title-text")
+                .attr("x", title1_x)
+                .attr("y", that.text_height * 0.6 + "px")
+                .attr("font-size", "20px")
+                .text("Label hierarchy");
+
+            
+            that.svg.append("text")
+                .attr("class", "title-text")
+                .attr("x", title2_x)
+                .attr("y", that.text_height * 0.6 + "px")
+                .attr("font-size", "20px")
+                .text("Image cluster");
 
             // treecut: dropdown-list
              let treecut_g = that.svg
@@ -523,9 +315,9 @@ export default {
                     (top_y)+")" + "scale(" + 1+"," + 1+")");
              treecut_g.append("text")
                 .attr("text-anchor", "start")
-                .attr("x", 14 + 2)
-                .attr("y", 20)
-                .attr("font-size", "18px")
+                .attr("x", 13)
+                .attr("y", 10)
+                .attr("font-size", "16px")
                 .text("Treecut");
              let members = [{
                   label: "None",
@@ -545,12 +337,12 @@ export default {
                 width: drop_down_width,
                 container: treecut_g,
                 members: members,
-                fontSize: 18,
+                fontSize: 14,
                 color: "#333",
-                fontFamily: "calibri",
-                x: 80,
-                y: 0,
-                 initMember:2,
+                fontFamily: "Roboto",
+                x: 0,
+                y: 15,
+                initMember:2,
                 changeHandler: function(option) {
                     if(option.label==="None") {
                         console.log("click tree cut", that.use_treecut);
@@ -608,56 +400,56 @@ export default {
              };
              svgDropDown(config);
 
-             // co-occurrence: dropdown-list
-            let cooccurence_x = start_x+drop_down_width+80+30;
-            let cooccurrence_dropdown_width = 150;
-            let cooccurence_g = that.svg
-                .append("g")
-                .attr("class", "current-label-checkbox")
-                .attr("transform", "translate("+
-                    (cooccurence_x)+","+
-                    (top_y)+")" + "scale(" + 1+"," + 1+")");
-             cooccurence_g.append("text")
-                .attr("text-anchor", "start")
-                .attr("x", 14 + 2)
-                .attr("y", 20)
-                .attr("font-size", "18px")
-                .text("Co-occurrence");
-             let cooccurrenceMembers = [{
-                  label: "Detected objects",
-                  value: 5
-                },
-                {
-                  label: "Extracted labels",
-                  value: 6
-                }
-             ];
+            //  // co-occurrence: dropdown-list
+            // let cooccurence_x = start_x+drop_down_width+80+30;
+            // let cooccurrence_dropdown_width = 150;
+            // let cooccurence_g = that.svg
+            //     .append("g")
+            //     .attr("class", "current-label-checkbox")
+            //     .attr("transform", "translate("+
+            //         (cooccurence_x)+","+
+            //         (top_y)+")" + "scale(" + 1+"," + 1+")");
+            //  cooccurence_g.append("text")
+            //     .attr("text-anchor", "start")
+            //     .attr("x", 14 + 2)
+            //     .attr("y", 20)
+            //     .attr("font-size", "18px")
+            //     .text("Co-occurrence");
+            //  let cooccurrenceMembers = [{
+            //       label: "Detected objects",
+            //       value: 5
+            //     },
+            //     {
+            //       label: "Extracted labels",
+            //       value: 6
+            //     }
+            //  ];
 
-             let cooccurrenceConfig = {
-                width: cooccurrence_dropdown_width,
-                container: cooccurence_g,
-                members: cooccurrenceMembers,
-                fontSize: 18,
-                color: "#333",
-                fontFamily: "calibri",
-                x: 140,
-                y: 0,
-                initMember:0,
-                changeHandler: function(option) {
-                    if(option.label==="Extracted labels") {
-                        console.log("select option: Extracted labels")
-                    } else if(option.label==="Detected objects") {
-                        console.log("select option: Detected objects")
-                    }
-                }
-             };
-             svgDropDown(cooccurrenceConfig);
+            //  let cooccurrenceConfig = {
+            //     width: cooccurrence_dropdown_width,
+            //     container: cooccurence_g,
+            //     members: cooccurrenceMembers,
+            //     fontSize: 18,
+            //     color: "#333",
+            //     fontFamily: "calibri",
+            //     x: 140,
+            //     y: 0,
+            //     initMember:0,
+            //     changeHandler: function(option) {
+            //         if(option.label==="Extracted labels") {
+            //             console.log("select option: Extracted labels")
+            //         } else if(option.label==="Detected objects") {
+            //             console.log("select option: Detected objects")
+            //         }
+            //     }
+            //  };
+            //  svgDropDown(cooccurrenceConfig);
 
             // precision & recall legend
             let precision_color = "rgb(201, 130, 206)";
             let recall_color = "rgb(79, 167, 255)";
             let rect_size = 6;
-            let precision_recall_legend_startx = cooccurence_x+cooccurrence_dropdown_width+140+50;
+            let precision_recall_legend_startx = title2_x + title2_len + 100;
             let pc_group = that.svg
                 .append("g")
                 .attr("class", "precision-recall-legend")
@@ -1185,6 +977,11 @@ export default {
 .mini-highlight {
     stroke: #5f5f5f;
     fill: none;
+}
+
+.title-text {
+    font-size: 20px;
+    font-weight: 600;
 }
 
 .main-content {
